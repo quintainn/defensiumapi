b6a68c380e78b600aea2e2d5ff1273e61b5fec24
José Quintinno <josequintino@hotmail.com.br>
2025-08-28 16:28:39 -0300

    feature: Implementar recuperação de usuários observando as regras de segregação de dados por sistema [#6]

---
+++ src/main/java/br/com/quintain/defensiumapi/filter/HibernateFilter.java
@@ -0,0 +1,43 @@
+package br.com.quintain.defensiumapi.filter;
+
+import java.io.IOException;
+
+import org.hibernate.Session;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.persistence.EntityManager;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class HibernateFilter extends OncePerRequestFilter {
+
+    private final EntityManager entityManager;
+
+    public HibernateFilter(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof Jwt jwt) {
+            Long sistemaID = jwt.getClaim("sistemaID");
+            if (sistemaID != null) {
+                entityManager.unwrap(Session.class)
+                        .enableFilter("sistemaFilter")
+                        .setParameter("id_sistema", sistemaID);
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+}
